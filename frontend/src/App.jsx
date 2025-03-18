d201946bb408c711c429836ae3a96606dc320832
animesh <animeshrathore255@gmail.com>
2025-03-18 12:11:57 +0530

    feat:created sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <>
       <Navbar />
+
+      <div className="grid grid-cols-12">
+
+        <div className=" col-span-2">
+        <Sidebar />
+        </div>
+        
+        <div className="col-span-10">
+
+        </div>
+    
+
+
+      </div>
+    
     </>
   );
 }
---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <div className="w-16 h-[568px] border p-1.5 border-gray-200">
+      <ul className="flex flex-col gap-y-5 items-center mt-12">
+        <li className="border border-gray-300 bg-gray-200 h-6 w-6"></li>
+        <li className="border border-gray-300 bg-gray-200 h-6 w-6"></li>
+        <li className="border border-gray-300 bg-gray-200 h-6 w-6"></li>
+        <li className="border border-gray-300 bg-gray-200 h-6 w-6"></li>
+        <li className="border border-gray-300 bg-gray-200 h-6 w-6"></li>
+        <li className="border border-gray-300 bg-gray-200 h-6 w-6"></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
